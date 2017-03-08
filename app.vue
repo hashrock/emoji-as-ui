3735c6a58991db61098400dd2390ec59e3eb28a3
hashrock <hashedrock@gmail.com>
2017-03-08 18:13:00 +0900

    init

---
+++ app.vue
@@ -0,0 +1,95 @@
+<template>
+    <div>
+        <div v-html="converted" class="output"></div>
+        <textarea v-model="msg"></textarea>
+    </div>
+</template>
+<style>
+    .emoji {
+        vertical-align: middle;
+        height: 22px;
+        cursor: pointer;
+    }
+
+
+    .output {
+        height: 22rem;
+        width: 30rem;
+        white-space: pre-wrap;
+    }
+
+
+    textarea {
+        height: 22rem;
+        width: 30rem;
+    }
+</style>
+
+<script>
+    function replaceNth(src, search, index, rep){
+        var nth = 0;
+        return src.replace(new RegExp(search, "g"), function (match, i, original) {
+            nth++;
+            return (nth === index) ? rep : match;
+        });
+    }
+    import * as Vue from "vue"
+    import * as emoji from "node-emoji"
+
+    function match(input, emojiName){
+        return input.indexOf(emoji.get(emojiName)) >= 0
+    }
+
+
+    function injectSpanItem(input, name){
+        return input.replace(new RegExp(emoji.get(name), 'g'), `<span class='emoji emoji-${name}'>${emoji.get(name)}</span>`)
+    }
+    function injectSpan(input){
+        input = injectSpanItem(input, "apple")
+        input = injectSpanItem(input, "pineapple")
+        /*
+        input = injectSpanItem(input, "white_medium_square")
+        input = injectSpanItem(input, "white_check_mark")
+        */
+        return input
+    }
+
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                msg: '',
+                converted: ""
+            }
+        },
+        watch: {
+            "msg": function () {
+                this.converted = emoji.emojify(this.msg)
+                this.converted = injectSpan(this.converted)
+
+                Vue.nextTick(() => {
+                    //イベントの追加
+                    //apple -> pineapple
+                    var emojis = document.querySelectorAll('.emoji-apple');
+                    for (let i = 0; i < emojis.length; i++) {
+                        emojis[i].addEventListener('click', (event)=> {
+                            this.msg = replaceNth(this.msg, ":apple:", i + 1, ":pineapple:")
+                        });
+                    }
+                    //pineapple -> apple
+                    var emojis = document.querySelectorAll('.emoji-pineapple');
+                    for (let i = 0; i < emojis.length; i++) {
+                        emojis[i].addEventListener('click', (event)=> {
+                            this.msg = replaceNth(this.msg, ":pineapple:", i + 1, ":apple:")
+                        });
+                    }
+                })
+            }
+        },
+        mounted: function () {
+            this.msg = "Welcome to Your Vue.js App\n:apple:\n\n:white_medium_square: TODO\n:white_check_mark: TODO2"
+        }
+    }
+
+</script>
